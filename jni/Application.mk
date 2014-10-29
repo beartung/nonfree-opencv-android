456b6ce5e07c3942f65660fa59db5b043d9fa655
Cypress Frankenfeld <iamcypress@gmail.com>
2014-10-29 15:40:00 -0400

    updated to work with opencv-2.4.9

--- jni/Application.mk
+++ jni/Application.mk
@@ -1,5 +1,4 @@
-APP_ABI := armeabi 
-APP_ABI += armeabi-v7a # you can do either armeabi or armeabi-v7a, steps are the same.
+APP_ABI := armeabi-v7a
 APP_STL := gnustl_static
 APP_CPPFLAGS := -frtti -fexceptions
-APP_PLATFORM := android-8
+APP_PLATFORM := android-15
